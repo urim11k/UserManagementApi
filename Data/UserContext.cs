75d718f7dbca2afbb19c7c96eca6aeeade277209
Urim Shkreli <125187111+urim11k@users.noreply.github.com>
2025-02-07 12:24:44 +1030

    Add files via upload

---
+++ Data/UserContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
